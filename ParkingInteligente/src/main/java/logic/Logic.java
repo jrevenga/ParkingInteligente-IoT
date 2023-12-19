ed06b26313ce0f3ec00b0f6bdf8b3fcdb23e38e9
Adrian Garcia <94239984+adigs24@users.noreply.github.com>
2023-12-19 14:09:50 +0100

    Update Logic.java

--- ParkingInteligente/src/main/java/logic/Logic.java
+++ ParkingInteligente/src/main/java/logic/Logic.java
@@ -465,15 +465,15 @@ public class Logic
 			Log.log.debug("Database Connected");
 			
 			PreparedStatement ps = ConectionDDBB.InsertnewMeasurement(con);
-			ps.setString(1, newTopic.getIdParking());
-			ps.setString(2, newTopic.getIdSensor());
-	        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-			ps.setString(3, sdf.format(timestamp));
-			ps.setString(4, newTopic.getValue());
-			ps.setString(5, newTopic.getIdParking());
-			ps.setString(6, newTopic.getIdSensor());
-			ps.setString(7, sdf.format(timestamp));
-			ps.setString(8, newTopic.getValue());
+			ps.setString(1, newTopic.getIdSensor());
+	        	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			ps.setString(2, sdf.format(timestamp));
+			ps.setString(3, newTopic.getValue());
+			ps.setString(4, newTopic.getAlarm());
+			ps.setString(5, newTopic.getIdSensor());
+			ps.setString(6, sdf.format(timestamp));
+			ps.setString(7, newTopic.getValue());
+			ps.setString(8, newTopic.getAlarm());
 			Log.log.info("Query to store Measurement=> {}", ps.toString());
 			ps.executeUpdate();
 		} catch (SQLException e)
